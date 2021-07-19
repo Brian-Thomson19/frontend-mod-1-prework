b37fb24d4beb7144bc3a65f7fb312b0de9f99def
Brian Thomson (he/him) <84817966+Brian-Thomson19@users.noreply.github.com>
2021-07-19 09:10:32 -0600

    Update dataTypes.js

--- section1/exercises/dataTypes.js
+++ section1/exercises/dataTypes.js
@@ -21,10 +21,10 @@ console.log("Alan Turing");
 console.log('Welcome to Turing!');
 
 // YOU DO: Write code below to log `99 bottles of pop on the wall...`:
-
+console.log('99 bottles of pop on the wall...');
 
 // YOU DO: Write code below to log one line from your favorite song or movie:
-
+console.log('Well, lets see. Skateboard... Truck Accident... Firehydrant. No, Not really. I skateboarded off a truck into a firehydrant.');
 
 
 //-------------------
