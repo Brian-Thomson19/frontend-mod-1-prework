0cb8dced7f400d227079691d6c832906aab41299
brian-thomson19 <bt.thomson@gmail.com>
2021-07-20 09:19:32 -0600

    made attempt at homework

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,12 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function printName() {
+  console.log("Oh, Hello!")
+}
+
+printName();
+printName();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,20 +40,40 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
-
+function printName() {
+  console.log("greetMe")
+  var inputOne = Kyle
+  var inputTwo = Tod
+  var inputThree = Tucket
+}
+if (inputOne === Kyle)
+if (inputTwo === Tod)
+if (inputThree === Tucker)
+}
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+var sasquach = 9
+var epic = 11
+return sasquch + epic
+return epic - sasquach
+PrintName();
+printName();
+printName();
+
 
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-
-
-
+var eric = "chill"
+var rick = "cool"
+if (eric === "chill", rick === "cool") {
+console.log('Eric and Rick are Bffs') }
+{ else if eric !== "chill"
+console.log('eric and rick are not bffs')}
 //-------------------
 // PART 3: Naming is Hard
 //-------------------
@@ -58,7 +84,7 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
-
+I named names of the specific people to designate the different between them. I think named each parameter specifically so that I would remember the different in them. I also made it something that ultimately make sense in my mind. 
 EXPLAIN:
 
 
