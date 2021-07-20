9a0716ce267aff5577ad5ddf989298e6eebdcfc9
brian-thomson19 <bt.thomson@gmail.com>
2021-07-20 10:23:30 -0600

    initial commit

--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -31,10 +31,10 @@ if (weather == 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
@@ -43,7 +43,13 @@ Log to the console the appropriate age range
 
 var dogAge = 3;
 // Write your conditional here
-
+if (dogAge <= 3) {
+  console.log('puppy')
+} if else (dogAge >= 10) {
+  console.log(elderly)
+} if else{
+  console.log(adult)
+}
 
 /*
 YOU DO:
@@ -65,6 +71,14 @@ When numQuarters = 3, program should log "I have enough money for a gumball"
 var numQuarters = 0;
 
 // Write your conditional here
+if numbQuarters < (2) {
+  console.log("i don't have enough money for a gumball")
+} if else numbQuarters >= (2) {
+  console.log("I have enough money for a gumball")
+}
+
+
+
 console.log("I have enough money for a gumball");
 console.log("I don't have enough money for a gumball");
 
@@ -86,11 +100,17 @@ var cupsOfFlour = 1;
 var hasSauce = true;
 
 // Write your conditional here
-
+if (cupsOfFlour < 2 && hasSauce) = false {
+  console.log('I cannot make a pizza')
+}if else (cupsOfFlour = 2&& hasSauce) = true {
+  console.log('I cannot make a pizza')
+} if else (cupsOfFlour >= 2 && hasSauce) = true {
+  console.log('I can make a pizza')
+}
 
 /*
-For the last two exercises, an ideal solution probably uses a logical operator. 
+For the last two exercises, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
-
+I absolutely did use them without even knowing. a logican operation is using the and or or not words to determine a sentence. 
 */
